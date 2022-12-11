ef08c5c3c53b44cadb2b7ea608a59cc0f070e926
reembaset <reembaset6@gmail.com>
2022-12-11 11:54:19 +0300

    this is with adding

---
+++ Design-main/Assets/Scripts/adding.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adding : MonoBehaviour
+{
+    public GameObject furn;
+    public Transform position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void create_furn()
+    {
+        Instantiate(furn, position);
+    }
+}
